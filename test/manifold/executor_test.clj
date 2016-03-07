1336175b7e296dd720144bdd314f51c3db413033
Michiel Eggermont <michiel.eggermont@gmail.com>
2016-03-07 19:51:55 +0100

    Simplify the test
    I realized that I don't need the filter-threads function to detect if
    my thread pool factory is being used. I can execute a task that
    puts the current thread name on a blocking queue and then take
    the name off the queue and compare the name to the expected name.

--- test/manifold/executor_test.clj
+++ test/manifold/executor_test.clj
@@ -1,33 +1,23 @@
 (ns manifold.executor-test
   (:require
     [clojure.test :refer :all]
-    [clojure.string :as str]
     [manifold.executor :as e])
   (:import
     [io.aleph.dirigiste
      Executors
      Executor]
-    [java.lang.management
-     ManagementFactory
-     ThreadInfo]))
-
-(defn filter-threads [name-prefix]
-  (let [thread-mx-bean (ManagementFactory/getThreadMXBean)
-        thread-infos (into [] (.getThreadInfo thread-mx-bean (.getAllThreadIds thread-mx-bean)))]
-    (filter (fn [^ThreadInfo info] (str/starts-with? (.getThreadName info) name-prefix)) thread-infos)))
+    [java.util.concurrent
+     LinkedBlockingQueue]))
 
 (deftest test-instrumented-executor-uses-thread-factory
-  (let [controller (Executors/utilizationController 0.95 1)
-        thread-count (atom 0)
-        pool-prefix "my-pool-prefix-"
-        factory (e/thread-factory
-                  #(str pool-prefix (swap! thread-count inc))
+  (let [thread-count (atom 0)
+        threadpool-prefix "my-pool-prefix-"
+        thread-factory (e/thread-factory
+                  #(str threadpool-prefix (swap! thread-count inc))
                   (deliver (promise) nil))
-        initial-thread-count 0
         executor (e/instrumented-executor
-                   {:controller           controller
-                    :thread-factory       factory
-                    :initial-thread-count initial-thread-count
-                    })]
-    (is (= false (.isShutdown ^Executor executor)))
-    (is (>= (count (filter-threads pool-prefix)) initial-thread-count))))
+                   {:controller     (Executors/utilizationController 0.95 1)
+                    :thread-factory thread-factory})
+        thread-names (LinkedBlockingQueue. 1)]
+    (.execute ^Executor executor #(.put thread-names (.getName (Thread/currentThread))))
+    (is (= (.take thread-names) (str threadpool-prefix @thread-count)))))
